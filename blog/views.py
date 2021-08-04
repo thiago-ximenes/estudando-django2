6863633bf97e5f430532ff62bb32005302a5423c
Thiago Ximenes Lima <50156388+thiagoximeneslima@users.noreply.github.com>
2021-08-03 22:32:14 -0300

    Create Post from admin is now possible

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        "author": "ThiagoXL",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "July 30, 2021",
-    },
-    {
-        "author": "MiguelLX",
-        "title": "Blog Post 2",
-        "content": "Second post content",
-        "date_posted": "July 29, 2021",
-    },
-]
+from .models import Post
 
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
